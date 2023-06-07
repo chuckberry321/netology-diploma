5d9b75cd78cc8a35eca202728d65455f1baa53af
Alexander Orekhov <chuckberry321@gmail.com>
2023-06-07 13:48:06 +0300

    some changes errors

--- terraform/cloud_user.tf
+++ terraform/cloud_user.tf
@@ -1,7 +1,7 @@
-#resource "tls_private_key" "tf_generated_private_key" {
-#  algorithm = "RSA"
-#  rsa_bits  = 4096
-#}
+resource "tls_private_key" "tf_generated_private_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
 
 resource "local_file" "cloud_user" {
   content = <<-DOC
@@ -13,31 +13,24 @@ users:
     sudo: ['ALL=(ALL) NOPASSWD:ALL']
     ssh_authorized_keys:
       - "${var.ssh_key}"
+      - "${tls_private_key.tf_generated_private_key.public_key_openssh}"
 DOC
   filename = "/tmp/cloud_user.txt"
   file_permission = "0600"      # Права доступа для пользователя
 
 
   depends_on = [
-     yandex_compute_instance.master,
-     yandex_compute_instance.worker
-#    tls_private_key.tf_generated_private_key
+    tls_private_key.tf_generated_private_key
   ]
 }
 
 
 resource "local_file" "private_key" {
-  content = "${var.ssh_key}"
+  content = tls_private_key.tf_generated_private_key.private_key_openssh
   filename = "/tmp/id_rsa_cloud_user"
   file_permission = "600"
 
-  provisioner "local-exec" {
-    command = "ls -la /tmp/ && cat /tmp/id_rsa_cloud_user"
-  }
-
-
   depends_on = [
-     yandex_compute_instance.master,
-     yandex_compute_instance.worker
+    tls_private_key.tf_generated_private_key
   ]
 }
