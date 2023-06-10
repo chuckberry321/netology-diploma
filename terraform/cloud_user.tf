c330037cab14a414ec9782495be7aacc0f4e5578
Alexander Orekhov <chuckberry321@gmail.com>
2023-06-10 20:03:23 +0300

    some changes errors

--- terraform/cloud_user.tf
+++ terraform/cloud_user.tf
@@ -18,11 +18,6 @@ DOC
   filename = "./meta.txt"
   file_permission = "600"      # Права доступа для пользователя
 
-  provisioner "local-exec" {
-    command = "cat ./meta.txt""
-  }
-
-
   depends_on = [
     tls_private_key.tf_generated_private_key
   ]
