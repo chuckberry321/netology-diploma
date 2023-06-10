6e6be98a73f7810b28b4dccf73ceb6c7d67a0e01
Alexander Orekhov <chuckberry321@gmail.com>
2023-06-10 20:23:34 +0300

    some changes errors

--- terraform/cloud_user.tf
+++ terraform/cloud_user.tf
@@ -1,4 +1,4 @@
-resource "tls_private_key" "tf_generated_private_key" {
+resource "tls_private_key" "tf_generate_key" {
   algorithm = "RSA"
   rsa_bits  = 4096
 }
@@ -13,22 +13,22 @@ users:
     sudo: ['ALL=(ALL) NOPASSWD:ALL']
     ssh_authorized_keys:
       - "${var.ssh_key}"
-      - "${tls_private_key.tf_generated_private_key.public_key_openssh}"
+      - "${tls_private_key.tf_generate_key.public_key_openssh}"
 DOC
   filename = "./meta.txt"
   file_permission = "600"      # Права доступа для пользователя
 
   depends_on = [
-    tls_private_key.tf_generated_private_key
+    tls_private_key.tf_generate__key
   ]
 }
 
 resource "local_file" "private_key" {
-  content = tls_private_key.tf_generated_private_key.private_key_openssh
+  content = tls_private_key.tf_generate_key.private_key_openssh
   filename = "/tmp/id_rsa_cloud_user"
   file_permission = "600"
 
   depends_on = [
-    tls_private_key.tf_generated_private_key
+    tls_private_key.tf_generate_key
   ]
 }
